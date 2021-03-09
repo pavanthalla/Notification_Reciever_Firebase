fd9d34e9dbeed4cbecba153f567cb8f32c1022f1
pavanthalla <pavanthalla012@gmail.com>
2021-03-09 15:28:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/notification_reciever/MainActivity.java
@@ -0,0 +1,132 @@
+package com.example.notification_reciever;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextView textView;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef;
+    RequestData rd1;
+
+    String staffid="staff2";
+
+    public String value;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView=findViewById(R.id.textView);
+
+        rd1=new RequestData();
+
+        myRef=database.getReference("staff").child(staffid);
+
+
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+              rd1 = snapshot.getValue(RequestData.class);
+
+                //textView.setText(rd1 != null?rd1.getMessage().toString():"Nodata");
+                value= Objects.requireNonNull(rd1).getMessage().toString();
+                textView.setText(value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+
+
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                notification();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+    }
+
+    private  void notification(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            NotificationChannel channel=new NotificationChannel("n","n", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager=getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+
+
+            Intent resultIntent = new Intent(this, MainActivity.class);
+
+            PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 1,
+                   resultIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+            NotificationCompat.Builder builder=new NotificationCompat.Builder(this,"n")
+                    .setContentText("PreSchool").setSmallIcon(R.drawable.ic_notification)
+                    .setContentText("Someone's Parent Arrived")
+                    .setContentIntent(resultPendingIntent).setAutoCancel(true);
+
+
+
+            NotificationManagerCompat managerCompat=NotificationManagerCompat.from(this);
+            managerCompat.notify(999,builder.build());
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/notification_reciever/RequestData.java
@@ -0,0 +1,16 @@
+package com.example.notification_reciever;
+
+public class RequestData {
+
+    private  String message;
+
+    public RequestData(){};
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
